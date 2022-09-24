25e28852a98abfc7cfc2d614b0a6896de134f1e6
BuiAnhTuan25 <tuantk7x@gmail.com>
2022-09-24 17:02:36 +0700

    add comment api

--- src/main/java/com/huce/doan/mxh/controller/CommentsController.java
+++ src/main/java/com/huce/doan/mxh/controller/CommentsController.java
@@ -1,11 +1,57 @@
 package com.huce.doan.mxh.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.huce.doan.mxh.model.dto.CommentsDto;
+import com.huce.doan.mxh.service.CommentsService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin
 @RestController
+@AllArgsConstructor
 @RequestMapping("api/v1.0/comments")
 public class CommentsController {
+    private final CommentsService commentsService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getComment(
+            @PathVariable Long id
+    ) {
+        return new ResponseEntity<>(commentsService.getComment(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/post-id/{id}")
+    public ResponseEntity<?> getByPostId(
+            @PathVariable Long id,
+            @RequestParam(name = "page") int page,
+            @RequestParam(name = "page-size") int pageSize
+    ) {
+        return new ResponseEntity<>(commentsService.getByPostId(id, page, pageSize), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> createComment(
+            @ModelAttribute CommentsDto comment,
+            @RequestParam(name = "picture", required = false) MultipartFile picture
+    ) {
+        return new ResponseEntity<>(commentsService.createComment(comment, picture), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateComment(
+            @PathVariable Long id,
+            @ModelAttribute CommentsDto comment,
+            @RequestParam(name = "picture", required = false) MultipartFile picture
+    ) {
+        return new ResponseEntity<>(commentsService.updateComment(comment, picture, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteComment(
+            @PathVariable Long id
+    ) {
+        return new ResponseEntity<>(commentsService.deleteComment(id), HttpStatus.OK);
+    }
 }
--- src/main/java/com/huce/doan/mxh/repository/CommentsRepository.java
+++ src/main/java/com/huce/doan/mxh/repository/CommentsRepository.java
@@ -1,9 +1,13 @@
 package com.huce.doan.mxh.repository;
 
 import com.huce.doan.mxh.model.entity.CommentsEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentsRepository extends JpaRepository<CommentsEntity, Long> {
+    Page<CommentsEntity> findByPostId(@Param("post_id") Long postId, Pageable pageable);
 }
